e96788c6679fd77ca81916672a294bab7de36a3f
YanaPlatonova <platon98yn@bk.ru>
2024-07-21 20:54:44 +0300

    store-func

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function ShopItemFunc({item}){
+
+    return(
+        <div class="main-content">
+          <h2>{item.brand}</h2>
+          <h1>{item.title}</h1>
+          <h3>{item.description}</h3>
+          
+          <div class="description">{item.descriptionFull}</div>
+          
+          <div class="highlight-window mobile">
+            <div class="highlight-overlay"/>
+          </div>
+          
+          <div class="divider"/>
+          
+          <div class="purchase-info">
+            <div class="price">{item.currency}{item.price}</div>
+            <button>Добавить в корзину</button>
+          </div>
+        </div>
+    );
+}
+
+export default ShopItemFunc;
